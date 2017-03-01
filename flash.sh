52dfe64a98dd1f716b52e6b0f656c852450f17d1
root <hallard>
2017-03-01 13:04:20 +0100

    Don't flash eep file

--- flash.sh
+++ flash.sh
@@ -1,31 +1,29 @@
-safeRunCommand() {
-   cmnd="$@" 
-   #$cmnd >/dev/null
-   $cmnd 
-   ERROR_CODE=$?
-   if [ ${ERROR_CODE} != 0 ]; then
-      printf "Error when executing command: '${command}'\n"
-   fi
-   return "${ERROR_CODE}"
+  safeRunCommand() {
+  cmnd="$@" 
+  #$cmnd >/dev/null
+  $cmnd 
+  ERROR_CODE=$?
+  if [ ${ERROR_CODE} != 0 ]; then
+    printf "Error when executing command: '${command}'\n"
+  fi
+  return "${ERROR_CODE}"
 }
 
 waitDevice() {
-	gotit=0
+  gotit=0
   echo "Waiting USB device to be plugged..."
-	while [ $gotit -eq 0 ]; do
-	
+  while [ $gotit -eq 0 ]; do
     # Scan USB device
     for DEV in /sys/bus/usb/devices/*; do
       if [[ "$(cat "$DEV/idVendor" 2>/dev/null)" == "0403" && "$(cat "$DEV/idProduct" 2>/dev/null)" == "6015" ]]; then
-			  gotit=1
-				DEVICE=$DEV
-			fi
-	  done
-		
-	done
-	# Force device redetection
-	echo "Found at $DEVICE"
-	echo 0 >$DEVICE/authorized
+        gotit=1
+        DEVICE=$DEV
+      fi
+    done
+  done
+  # Force device redetection
+  echo "Found at $DEVICE"
+  echo 0 >$DEVICE/authorized
   echo 1 >$DEVICE/authorized
 }
 
@@ -55,48 +53,48 @@ else
 fi
 
 while true; do
-	#default not OK
+  #default not OK
   OK=1
-	
+  
   echo "-------------------------------------------"
   echo "Searching for USB Dongle with FT230-X CHIP "
   echo "will be flashed with serial TELEINFO-$SERNUM"
   echo "-------------------------------------------"
-	waitDevice
+  waitDevice
 
-	# Program USB Dongle with default values
-	echo "-------------------------------------------"
-	echo "Programming USB Dongle with defaults values"
-	echo "-------------------------------------------"
-	#command="./ftx_prog --new-serial-number TINFO-$NSERNUM --cbus 1 PWREN --cbus 2 TxLED --invert rxd"
-	safeRunCommand "./ftx_prog --restore teleinfo.eep"
-	retval=$?
-	# reset USB dongle, LED should be RED
-	usb_modeswitch -v 0x0403 -p 0x6015 --reset-usb --quiet
-	waitDevice
-	if [ "$retval" == 0 ] ; then
-		printOK
-		echo "-------------------------------------------"
-		echo "      Checking Send and Receive data       "
-		echo "-------------------------------------------"
-		safeRunCommand "python ./serial_check.py"
-		retval=$?
-		if [ "$retval" == 0 ] ; then
-			printOK
-			echo "-------------------------------------------"
-			echo "Flashing module with serial number $SERNUM"
-			echo "-------------------------------------------"
-			safeRunCommand "./ftx_prog --new-serial-number TINFO-$SERNUM --cbus 1 PWREN --cbus 2 TxLED --invert rxd"
-			retval=$?
-			# reset USB dongle, LED should be GREEN
-			usb_modeswitch -v 0x0403 -p 0x6015 --reset-usb --quiet
-			waitDevice
-			if [ "$retval" == 0 ] ; then
-				printOK
-				OK=0
-			fi
-		fi
-	fi
+  # Program USB Dongle with default values
+  echo "-------------------------------------------"
+  echo "Programming USB Dongle with defaults values"
+  echo "-------------------------------------------"
+  safeRunCommand "./ftx_prog --new-serial-number TINFO-0000 --cbus 2 PWREN --cbus 1 TxLED --invert rxd"
+  #safeRunCommand "./ftx_prog --restore teleinfo.bin"
+  retval=$?
+  # reset USB dongle, LED should be RED
+  usb_modeswitch -v 0x0403 -p 0x6015 --reset-usb --quiet
+  waitDevice
+  if [ "$retval" == 0 ] ; then
+    printOK
+    echo "-------------------------------------------"
+    echo "      Checking Send and Receive data       "
+    echo "-------------------------------------------"
+    safeRunCommand "python ./serial_check.py"
+    retval=$?
+    if [ "$retval" == 0 ] ; then
+      printOK
+      echo "-------------------------------------------"
+      echo "Flashing module with serial number $SERNUM"
+      echo "-------------------------------------------"
+      safeRunCommand "./ftx_prog --new-serial-number TINFO-$SERNUM --cbus 1 PWREN --cbus 2 TxLED --noinvert rxd"
+      retval=$?
+      # reset USB dongle, LED should be GREEN
+      usb_modeswitch -v 0x0403 -p 0x6015 --reset-usb --quiet
+      waitDevice
+      if [ "$retval" == 0 ] ; then
+        printOK
+        OK=0
+      fi
+    fi
+  fi
   
   if [[ $OK -ne 0 ]]; then
     printERROR
@@ -115,7 +113,7 @@ while true; do
   while read -r -t 0; do
     read -n 256 -r -s
   done
-	
+  
   read -p "press Enter to continue or q to quit " rep
   if [[ $rep == "Q" || $rep == "q" ]]; then
     echo ""
